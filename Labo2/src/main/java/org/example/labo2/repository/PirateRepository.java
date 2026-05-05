0a68f42d50bc792f7759e5a2a6e6ccc167c65c60
Juanbermudezp <00218123@uca.edu.sv>
2026-05-04 18:05:53 -0600

    :memo: Modificacion de Clase a Interfas del repositorio e inclusion del metodo

--- Labo2/src/main/java/org/example/labo2/repository/PirateRepository.java
+++ Labo2/src/main/java/org/example/labo2/repository/PirateRepository.java
@@ -1,4 +1,8 @@
 package org.example.labo2.repository;
 
-public class PirateRepository {
+import org.example.labo2.domain.entity.Pirate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface PirateRepository extends JpaRepository<Pirate, UUID> {
 }
