a816572fe40c8039f1f5b4363db1101c4e4e7415
Juanbermudezp <00218123@uca.edu.sv>
2026-05-04 18:37:59 -0600

    :memo: Modificacion de la Interfaz

--- Labo2/src/main/java/org/example/labo2/service/PirateService.java
+++ Labo2/src/main/java/org/example/labo2/service/PirateService.java
@@ -1,4 +1,14 @@
 package org.example.labo2.service;
 
+import org.example.labo2.domain.entity.Pirate;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface PirateService {
+    Pirate save(Pirate pirate);
+    List<Pirate> findAll();
+    Pirate findById(UUID id);
+    Pirate update(UUID id, Pirate pirate);
+    void delete(UUID id);
 }
