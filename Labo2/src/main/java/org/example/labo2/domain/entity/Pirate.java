16ef007585c66ae15a6392fe5f8e24c08366ecef
Juanbermudezp <00218123@uca.edu.sv>
2026-05-04 18:01:22 -0600

    :memo: Modificacion de la entidad Pirata

--- Labo2/src/main/java/org/example/labo2/domain/entity/Pirate.java
+++ Labo2/src/main/java/org/example/labo2/domain/entity/Pirate.java
@@ -1,4 +1,26 @@
 package org.example.labo2.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "pirates")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pirate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    private Double bounty;
+    private String crew;
+    private Boolean isAlive;
 }
